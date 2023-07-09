33d4f011e7e712e5dd26d2acb085fad7442bb0e6
Tazzok <131045020+Tazzok@users.noreply.github.com>
2023-07-09 18:54:38 -0500

    Login and Sign up working

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,6 @@ const apiRoutes = require('./api');
 const viewRoutes = require('./viewRoutes');
 
 router.use('/api', apiRoutes);
-router.use('/', viewRoutes)
+// router.use('/', viewRoutes)
 
 module.exports = router;
